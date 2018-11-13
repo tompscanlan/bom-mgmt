d9377544367213aec9664ef281b21a5ccc28373c
Tom Scanlan <tscanlan@vmware.com>
2018-11-13 15:36:17 -0500

    add intial travis build

--- commands/version.go
+++ commands/version.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 )
 
-var VERSION = "1.0.0"
+var VERSION = "1.0.6"
+var GitRev = ""
 
 type VersionCommand struct {
 }
 
 //Execute - returns the version
 func (c *VersionCommand) Execute([]string) error {
-	fmt.Println(VERSION)
+	suffix := ""
+	if GitRev != "" {
+		suffix = fmt.Sprintf("-rev-%s\n", GitRev)
+	}
+	fmt.Println(fmt.Sprintf("%s%s",VERSION, suffix))
 	return nil
 }
